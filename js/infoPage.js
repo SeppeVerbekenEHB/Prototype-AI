9341fadd1dd777bf1414bb39db3c0484bd86ad6a
Seppe <seppe.verbeken@student.ehb.be>
2024-05-07 11:08:07 +0200

    Add an image for each fruit/vegetable in the model

--- js/infoPage.js
+++ js/infoPage.js
@@ -12,12 +12,34 @@ document.addEventListener('DOMContentLoaded', function () {
     const fiber = urlParams.get('fiber');
 
     // Display the retrieved data on the page
-    document.getElementById('name').innerHTML = "<strong>Nutritional Information for</strong> " + name;
+    document.getElementById('name').innerHTML = "<strong>" + name + "</strong";
     document.getElementById('calories').innerHTML = "<strong>Calories:</strong> " + calories;
     document.getElementById('protein').innerHTML = "<strong>Protein:</strong> " + protein + "g";
     document.getElementById('carbohydrates').innerHTML = "<strong>Carbohydrates:</strong> " + carbs + "g";
     document.getElementById('fat').innerHTML = "<strong>Fat:</strong> " + fat + "g";
     document.getElementById('fiber').innerHTML = "<strong>Fiber:</strong> " + fiber + "g";
+
+    const image = document.getElementById('image');
+    // Set the src attribute based on the detected fruit/vegetable
+    switch (name.toLowerCase()) {
+        case 'apple':
+            image.src = '../images/apple.jpg';
+            break;
+        case 'banana':
+            image.src = '../images/banana.jpg';
+            break;
+        case 'broccoli':
+            image.src = '../images/broccoli.jpg';
+            break;
+        case 'carrot':
+            image.src = '../images/carrot.jpg';
+            break;
+        case 'orange':
+            image.src = '../images/orange.jpg';
+            break;
+        default:
+            image.src = '../images/default.jpg';
+    }
 });
 
 // Add event listener to the go back button
